5c6df56623c94b5fa74f31e3134e15a9ff1e6b43
Nikusha Jvarsheishvili <nikushajvarsheishvili28@gmail.com>
2024-07-02 03:16:55 +0400

    i did form of step one

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,105 @@
 import "./App.css";
+import bgSidebarDesktop from "./assets/images/bg-sidebar-desktop.svg";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { object, number, string } from "yup";
+import { useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(1);
+
+  interface Form {
+    name: string;
+    email: string;
+    phoneNumber: number;
+  }
+
+  const handleSubmit = (values: Form) => {
+    console.log(values);
+  };
+  const initialValues = {
+    name: "",
+    email: "",
+    phoneNumber: "",
+  };
+
+  const steps = ["your info", "select plane", "add-ons", "summary"];
+
   return (
-    <>
-      <h1>React and Tailwind Starter</h1>
-    </>
+    <div className="p-4 bg-white rounded-lg flex  max-w-60">
+      <div className="relative min-w-17">
+        <img src={bgSidebarDesktop} alt="" />
+        <div className="p-10 flex flex-col gap-5 absolute top-0 w-full h-80">
+          {steps.map((item, index) => {
+            const stepIndex = index + 1;
+            return (
+              <div className="flex gap-5  items-center" key={index}>
+                <div
+                  className={`border rounded-full text-white w-10 h-10 text-center p-1.5 font-medium ${
+                    stepIndex === step ? "stepActive" : ""
+                  }`}
+                >
+                  {stepIndex}
+                </div>
+                <div>
+                  <p className="text-coolGray text-sm">STEP {stepIndex}</p>
+                  <h3 className="uppercase font-bold text-white">{item}</h3>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className=" flex flex-col gap-10 pl-28 py-8">
+        <div>
+          <h2 className="font-bold text-3xl text-marineBlue">Personal Info</h2>
+          <p className="font-medium text-coolGray my-1">
+            Please provide your name, email address, and phone number.
+          </p>
+        </div>
+        <Formik onSubmit={handleSubmit} initialValues={initialValues}>
+          {(formik) => {
+            return (
+              <Form className="flex flex-col gap-5">
+                <label className="labelInput label">
+                  <span>Name</span>
+                  <Field
+                    className="input focus:outline-inputActive"
+                    name="name"
+                    type="text"
+                    placeholder="e.g. Stephen King"
+                  />
+                </label>
+                <label className="labelInput label">
+                  <span>Email Address</span>
+                  <Field
+                    className="input focus:outline-inputActive"
+                    name="email"
+                    type="text"
+                    placeholder="e.g. stephenking@lorem.com"
+                  />
+                </label>
+                <label className="labelInput label">
+                  <span>Phone Number</span>
+                  <Field
+                    className="input focus:outline-inputActive"
+                    name="phoneNumber"
+                    type="number"
+                    placeholder="e.g. +1 234 567 890"
+                  />
+                </label>
+                <button
+                  onClick={() => setStep(step + 1)}
+                  type="submit"
+                  className="ms-auto border mt-12 bg-marineBlue text-white px-5 py-3 font-medium rounded-lg hover:bg-buttonHover transition-all duration-300"
+                >
+                  Next Step
+                </button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </div>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,54 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        marineBlue: "hsl(213, 96%, 18%)",
+        purplishBlue: "hsl(243, 100%, 62%)",
+        pastelBlue: "hsl(228, 100%, 84%)",
+        lightBlue: "hsl(206, 94%, 87%)",
+        strawberryRed: "hsl(354, 84%, 57%)",
+        coolGray: "hsl(231, 11%, 63%)",
+        lightGray: "hsl(229, 24%, 87%)",
+        magnolia: "hsl(217, 100%, 97%)",
+        alabaster: "hsl(231, 100%, 99%)",
+        white: "hsl(0, 0%, 100%)",
+        buttonHover: "#164a8a",
+        inputActive: "#887fd6",
+      },
+      minWidth: {
+        17: "17rem",
+      },
+      maxWidth: {
+        60: "60rem",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".stepActive": {
+          backgroundColor: "hsl(206, 94%, 87%)",
+          color: "black",
+          transition: "all 0.4s",
+        },
+        ".labelInput": {
+          display: "flex",
+          flexDirection: "column",
+          fontWeight: "500",
+        },
+        ".input": {
+          border: "1px solid hsl(229, 24%, 87%)",
+          borderRadius: "0.5rem",
+          padding: "0.75rem",
+          marginTop: "7px",
+        },
+        ".label": {
+          color: "hsl(213, 96%, 18%)",
+        },
+      };
+      addUtilities(newUtilities, ["responsive", "hover"]);
+    },
+  ],
+};
